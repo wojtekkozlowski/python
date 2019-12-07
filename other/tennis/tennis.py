a94739d6f47e9fb3f922a1d9017b9acf72b64e6e
woj <maniacus.gd@gmail.com>
2019-12-07 12:20:05 +1100

    refactoring

--- other/tennis/tennis.py
+++ other/tennis/tennis.py
@@ -1,6 +1,3 @@
-from collections import namedtuple
-
-
 class TennisGame:
     p1 = "p1"
     p2 = "p2"
@@ -21,21 +18,20 @@ class TennisGame:
         self.__score_history.append(self.p2)
 
     @staticmethod
-    def player_scored(player_score, opponent_score, player_games_won, opponent_games_won):
+    def player_scored(player_score, opponent_score, player_games_won):
         if player_score < 30:
-            return (player_score + 15, opponent_score), (player_games_won, opponent_games_won)
-        else:
-            if player_score == 40:
-                if opponent_score == 45:
-                    return (player_score, 40), (player_games_won, opponent_games_won)
-                elif opponent_score == 40:
-                    return (player_score + 5, opponent_score), (player_games_won, opponent_games_won)
-                else:
-                    return (0, 0), (player_games_won + 1, opponent_games_won)
-            elif player_score == 45:
-                return (0, 0), (player_games_won + 1, opponent_games_won)
+            return (player_score + 15, opponent_score), player_games_won
+        elif player_score == 30:
+            return (player_score + 10, opponent_score), player_games_won
+        elif player_score == 40:
+            if opponent_score == 45:
+                return (player_score, 40), player_games_won
+            elif opponent_score == 40:
+                return (player_score + 5, opponent_score), player_games_won
             else:
-                return (player_score + 10, opponent_score), (player_games_won, opponent_games_won)
+                return (0, 0), player_games_won + 1
+        else:
+            return (0, 0), player_games_won + 1
 
     def get_games_won(self):
         return self.__p1_games_won, self.__p2_games_won
@@ -43,16 +39,14 @@ class TennisGame:
     def get_score(self):
         for s in self.__score_history:
             if s == self.p1:
-                res = self.player_scored(self.__p1_score, self.__p2_score, self.__p1_games_won, self.__p2_games_won)
+                res = self.player_scored(self.__p1_score, self.__p2_score, self.__p1_games_won)
                 self.__p1_score = res[0][0]
                 self.__p2_score = res[0][1]
-                self.__p1_games_won = res[1][0]
-                self.__p2_games_won = res[1][1]
+                self.__p1_games_won = res[1]
             else:
-                res = self.player_scored(self.__p2_score, self.__p1_score, self.__p2_games_won, self.__p1_games_won)
-                self.__p1_score = res[0][1]
+                res = self.player_scored(self.__p2_score, self.__p1_score, self.__p2_games_won)
                 self.__p2_score = res[0][0]
-                self.__p1_games_won = res[1][1]
-                self.__p2_games_won = res[1][0]
+                self.__p1_score = res[0][1]
+                self.__p2_games_won = res[1]
 
         return self.__p1_score, self.__p2_score
